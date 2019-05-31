486ea1c4d5faef92f2cfbbf839119a02e1668320
Igor Spasić <igor@jodd.org>
2019-05-31 20:54:18 +0200

    Date fixed

--- src/nunjucks.js
+++ src/nunjucks.js
@@ -1,25 +1,8 @@
-"use strict";
-
-const datetimefmt = require('../spig/filters/datetimefmt');
-const pages = require('../spig/filters/pages');
-const collections = require('../spig/filters/collections');
+const rs = require('./filters/rs');
 
 module.exports = nunjucksEnv => {
   nunjucksEnv
-    .addFilter('out', require('../spig/filters/out'))
-
-    .addFilter('dateDisplay', datetimefmt.dateDisplay)
-    .addFilter('dateISO', datetimefmt.dateISO)
-    .addFilter('dateUTC', datetimefmt.dateUTC)
-
-    .addFilter('within', pages.within)
-    .addFilter('reverse', pages.reverse)
-    .addFilter('sortBy', pages.sortBy)
-    .addFilter('groupBy', pages.groupBy)
-    .addFilter('groupByYear', pages.groupByDateYear)
-    .addFilter('lastN', pages.lastN)
-    .addFilter('firstN', pages.firstN)
-
-    .addFilter('keys', collections.keys)
+    .addFilter('rsDateShort', rs.dateShort)
+    .addFilter('rsDateLong', rs.dateLong)
   ;
 };
